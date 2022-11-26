832c5eb3653947cfa0582facdd4197864898e138
Aaditya0110 <113664144+Aaditya0110@users.noreply.github.com>
2022-11-26 17:03:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactExample from './ck2/ui';
+
+function App() {
+  return (
+    <ReactExample />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import ResponsiveAppBar from './components/nav';
+// import BasicCard from './components/cards';
+// import MediaControlCard from './components/card2';
+// import XOgame from './xo';
+// import Choose from './choose';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
